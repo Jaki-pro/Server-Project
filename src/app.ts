5444cac30359190dc9539050a7adcf05b8c1b0d1
Jakaria Hossain <mdjak8980@gmail.com>
2024-05-17 22:18:31 +0600

    sample server

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 const app: Application = express();
 app.use(express.json());
 app.use(cors());
+const a = 10;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World beta ka');
